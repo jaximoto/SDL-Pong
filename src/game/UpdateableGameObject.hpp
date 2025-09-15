267c377dd1383503bdfae1958d61f93dc2a9c70d
Jaxon Simmons <62913915+jaximoto@users.noreply.github.com>
2025-09-15 09:00:41 -0700

    test push

--- src/game/UpdateableGameObject.hpp
+++ src/game/UpdateableGameObject.hpp
@@ -7,6 +7,7 @@ class UpdateableGameObject : public GameObject, public Updateable
 	public:
 		using GameObject::GameObject; // Inherit constructors directly
 
+		// Still need a virtual destructor for proper cleanup of derived classes
 		virtual ~UpdateableGameObject() = default;
 		virtual void update(float delta_time) = 0;
 
