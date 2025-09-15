ada2dd4be215e8e25f63c82906db4343ee5b5e5d
yibbyyy <106987734+yibbyyy@users.noreply.github.com>
2025-09-15 14:38:59 -0700

    added vector2 struct

---
+++ src/game/Vector2.hpp
@@ -0,0 +1,57 @@
+#pragma once
+#include <string>
+#include <sstream>
+//This namespace should get renamed lol
+//I dont think i can actually do it like this but i want to put something down and this feels intuitively how this would work
+struct vector2 {
+
+	public:
+		//x and y components of the vector
+		float x;
+		float y;
+		//standard constructor accepts float for parameters x, y
+		constexpr vector2(float x = 0.0f, float y = 0.0f) : x(x), y(y) {};
+
+		//converts vector2 to string in (x,y) format
+		std::string toString() const {
+			std::ostringstream ss;
+			ss << "(" << x << "," << y << ")";
+			return ss.str();
+		}
+	
+		//directional set and standard vectors
+		static const vector2 zero;
+		static const vector2 one;
+		static const vector2 up;
+		static const vector2 down;
+		static const vector2 left;
+		static const vector2 right;
+
+		//vector + vector
+		vector2 operator+(const vector2& other) {
+			return vector2(x + other.x, y + other.y);
+		}
+		//vector - vector
+		vector2 operator-(const vector2& other) {
+			return vector2(x - other.x, y - other.y);
+		}
+		//vector multiply by scalar
+		vector2 operator*(float scalar) {
+			return vector2(x * scalar, y * scalar);
+		}
+		//what other things may we need
+		//dot product
+		//cross product
+		//standard directional vectors
+		//normalized
+		//magnitude
+		//lerp
+		//move towards
+};
+//maps standard and directional vectors
+const vector2 vector2::zero = vector2(0.0f, 0.0f);
+const vector2 vector2::one = vector2(1.0f, 1.0f);
+const vector2 vector2::up = vector2(0.0f, 1.0f);
+const vector2 vector2::down = vector2(0.0f, -1.0f);
+const vector2 vector2::left = vector2(-1.0f, 0.0f);
+const vector2 vector2::right = vector2(1.0f, 0.0f);
