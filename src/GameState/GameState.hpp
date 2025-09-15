1ea3dc4d840003299d897fab28e2fcafa1b8459b
Jaxon Simmons <62913915+jaximoto@users.noreply.github.com>
2025-09-15 10:38:10 -0700

    sync with other pc

---
+++ src/GameState/GameState.hpp
@@ -0,0 +1,10 @@
+#pragma once
+#include <SDL3/SDL.h>
+class GameState
+{
+public:
+	SDL_Renderer* renderer;
+	SDL_Window* window;
+
+	GameState(SDL_Renderer* renderer, SDL_Window* window, int width, int height, SD)
+};
